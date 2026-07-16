51415a5cf534090b7c86acbf1271e470306557a5
Mukesh Patel <mpatel125@outlook.com>
2026-07-16 21:26:46 +0100

    First hello world claude interaction working

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
-import anthropic
+import os
+from anthropic import Anthropic
+from dotenv import load_dotenv
 
-client = anthropic.Anthropic()
+load_dotenv()
+
+client = Anthropic(api_key=os.environ.get("API_KEY"))
+
+messages = client.messages.create(
+    model="claude-haiku-4-5",
+    max_tokens=1024,
+    messages=[{
+        "role": "user",
+        "content": "Hello, Claude"
+    }]
+)
+
+for message in messages:
+    print(message)
